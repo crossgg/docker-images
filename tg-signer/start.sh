6b4603d46616de296511af54adb261fbc9831e41
crosscc <hzoo@vip.qq.com>
2026-01-20 18:07:36 +0800

    1

--- tg-signer/start.sh
+++ tg-signer/start.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
+# 首次配置时使用，后续注释掉
 pip install -U tg-signer
+pip install "tg-signer[gui]"
 
 # 首次配置时使用，后续注释掉
 sleep infinity
 
 # 配置完成后取消注释
 # tg-signer run mytasks
+
+nohup tg-signer webgui -H 0.0.0.0 >/dev/null &     
